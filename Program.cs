c2f44b44251853ef232131c26e122c4fd1a93f8c
Aidan <jaidanstacey@gmail.com>
2022-06-08 17:06:35 -0600

    Finished Tic Tac Toe Setup

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Week1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string squareOne = "1";
+            string squareTwo = "2";
+            string squareThree = "3";
+            string squareFour = "4";
+            string squareFive = "5";
+            string squareSix = "6";
+            string squareSeven = "7";
+            string squareEight = "8";
+            string squareNine = "9";
+
+
+            Console.WriteLine($"{squareOne}|{squareTwo}|{squareThree}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{squareFour}|{squareFive}|{squareSix}");
+            Console.WriteLine("-+-+-");
+            Console.WriteLine($"{squareSeven}|{squareEight}|{squareNine}");
+
+        }
+    }
+}
